6b1a378f71b57f869f8925a720f11695e0d77f4b
Michael Jang <michaeljang86@gmail.com>
2022-05-02 16:42:27 -0400

    Basic layout with Demo Banner
    No functionality yet, just generic layout and a banner that gives you a tip for testing the functionality.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
+import DemoBanner from "./DemoBanner";
+import Chatroom from "./Chatroom";
+
 function App() {
+  const [demoBannerDisplayed, setDemoBannerDisplayed] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Welcome to Hooked On FireChat</h1>{" "}
       </header>
+      {demoBannerDisplayed && (
+        <DemoBanner onClose={() => setDemoBannerDisplayed(false)} />
+      )}
+      <div className="chatrooms">
+        <Chatroom roomId={3} />
+        <Chatroom roomId={3} />
+      </div>
     </div>
   );
 }
---
+++ src/DemoBanner.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function DemoBanner(props) {
+  return (
+    <DemoBannerWrapper classname="warning">
+      <BannerHeading>DEMO MODE</BannerHeading>
+      <p>
+        NOTE: To get a real sense of the chatroom functionality, open this app
+        in two browser windows so you can simulate being two people.
+      </p>
+      <CloseButton onClick={props.onClose}>X</CloseButton>
+    </DemoBannerWrapper>
+  );
+}
+
+const DemoBannerWrapper = styled.div`
+  position: relative;
+
+  width: 75%;
+  height: 100px;
+
+  margin: auto;
+  padding: 10px;
+
+  border: 2px solid;
+  border-radius: 10px;
+
+  background: pink;
+
+  text-align: left;
+  color: red;
+`;
+
+const BannerHeading = styled.h3`
+  text-align: center;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  width: revert; // This is because I plan to set a global size for buttons. Might not turn out to be necessary.
+  cursor: pointer;
+  background: none;
+  border: none;
+  color: inherit;
+`;
